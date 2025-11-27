a3badf1e69b3ba998b27e408c6c48af99280cbfd
Shazadkhan1999 <Shazadkhan40005@gmail.com>
2025-11-28 00:00:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function volume_sphere() {
     //Write your code here
-  
+
+	let r = document.getElementById("radius").value;
+	if(r == "" || r <= 0){
+		/alert("please enter valid radius")
+	}
+  let volume = (4 / 3) * Math.PI * Math.pow(r, 3);
+
+    document.getElementById("volume").value = volume.toFixed(4);
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
